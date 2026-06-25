ad124a699bdbdf902f03b6561bd3e16fbb3f628a
jay sheth <jsheth066@gmail.com>
2026-06-25 18:18:05 +0400

    UPD: config files

---
+++ TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using test.Shared.Exceptions;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace test;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        throw new NotFoundException(
+            "Test not found exception");
+    }
+}
