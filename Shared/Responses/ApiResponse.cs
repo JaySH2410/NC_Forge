d7d059ceb3bff962e91a142e394d27147eed43ed
jay sheth <jsheth066@gmail.com>
2026-06-25 18:18:28 +0400

    ADD: default response structure

---
+++ Shared/Responses/ApiResponse.cs
@@ -0,0 +1,64 @@
+﻿namespace test.Shared.Responses;
+
+public class ApiResponse
+{
+    public bool IsSuccess { get; init; }
+
+    public string? TraceId { get; set; }
+
+    public string Message { get; init; } = string.Empty;
+
+    public Dictionary<string, string[]>? Errors { get; init; }
+
+    public DateTimeOffset Timestamp { get; init; }
+
+    public static ApiResponse Success(string message)
+    {
+        return new ApiResponse
+        {
+            IsSuccess = true,
+            Message = message,
+            Timestamp = DateTimeOffset.UtcNow
+        };
+    }
+
+    //public static ApiResponse Failure(
+    //    string message,
+    //    Dictionary<string, string[]>? errors = null)
+    //{
+    //    return new ApiResponse
+    //    {
+    //        IsSuccess = false,
+    //        Message = message,
+    //        Errors = errors,
+    //        TraceId = null,
+    //        Timestamp = DateTimeOffset.UtcNow
+    //    };
+    //}
+}
+
+public class ApiResponse<T>
+{
+    public bool IsSuccess { get; init; }
+
+    public string? TraceId { get; set; }
+
+    public string Message { get; init; } = string.Empty;
+
+    public T? Data { get; init; }
+
+    public DateTimeOffset Timestamp { get; init; }
+
+    public static ApiResponse<T> Success(
+        T data,
+        string message)
+    {
+        return new ApiResponse<T>
+        {
+            IsSuccess = true,
+            Message = message,
+            Data = data,
+            Timestamp = DateTimeOffset.UtcNow
+        };
+    }
+}
---
+++ Shared/Responses/PagedResult.cs
@@ -0,0 +1,27 @@
+﻿namespace test.Shared.Responses;
+
+public class PagedResult<T>
+{
+    public IReadOnlyCollection<T> Items { get; init; } = [];
+
+    public int Page { get; init; }
+
+    public int PageSize { get; init; }
+
+    public int TotalRecords { get; init; }
+
+    public int TotalPages { get; init; }
+
+    public PagedResult(
+        IReadOnlyCollection<T> items,
+        int page,
+        int pageSize,
+        int totalRecords)
+    {
+        Items = items;
+        Page = page;
+        PageSize = pageSize;
+        TotalRecords = totalRecords;
+        TotalPages = (int)Math.Ceiling(totalRecords / (double)pageSize);
+    }
+}
