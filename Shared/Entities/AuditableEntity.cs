7768aace914e74a8a41cc1c1a639b9b68388c02c
jay sheth <jsheth066@gmail.com>
2026-06-25 23:31:42 +0400

    ADD: auditable entity

---
+++ Shared/Entities/AuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace test.Shared.Entities;
+
+public abstract class AuditableEntity : BaseEntity
+{
+    public DateTimeOffset CreatedAt { get; set; }
+
+    public int? CreatedBy { get; set; }
+
+    public DateTimeOffset? UpdatedAt { get; set; }
+
+    public int? UpdatedBy { get; set; }
+}
