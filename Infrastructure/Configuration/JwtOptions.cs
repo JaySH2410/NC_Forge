86efd5da840ddc21263e390e8c2d1e68aad0825e
jay sheth <jsheth066@gmail.com>
2026-06-26 17:52:45 +0400

    ADD: JWT

---
+++ Infrastructure/Configuration/JwtOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace test.Infrastructure.Configuration;
+
+public sealed class JwtOptions
+{
+    public const string SectionName = "Jwt";
+
+    public required string Secret { get; init; }
+
+    public required string Issuer { get; init; }
+
+    public required string Audience { get; init; }
+
+    public int AccessTokenExpiryMinutes { get; init; }
+
+    public int RefreshTokenExpiryDays { get; init; }
+}
