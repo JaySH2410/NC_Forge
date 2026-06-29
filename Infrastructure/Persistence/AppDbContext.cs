b9c18393d9c76906c6ad3bd45a6b22a74fd42573
jay sheth <jsheth066@gmail.com>
2026-06-29 13:12:31 +0400

    UPD: refreshToken added

--- Infrastructure/Persistence/AppDbContext.cs
+++ Infrastructure/Persistence/AppDbContext.cs
@@ -12,6 +12,7 @@ public class AppDbContext : DbContext
         : base(options)
     { }
     public DbSet<User> Users => Set<User>();
+    public DbSet<RefreshToken> RefreshTokens =>  Set<RefreshToken>();
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
