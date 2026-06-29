b678cbd5fe7f074336e355786af29db1b635ca96
jay sheth <jsheth066@gmail.com>
2026-06-29 17:03:57 +0400

    UPD: EmailVerification entity and mapping it with user

--- Features/Auth/Entities/User.cs
+++ Features/Auth/Entities/User.cs
@@ -16,4 +16,5 @@ public class User : AuditableEntity
     public DateTimeOffset? LastLoginAt { get; set; }
     public ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
     public ICollection<PasswordResetToken> PasswordResetTokens { get; set; } = new List<PasswordResetToken>();
+    public ICollection<EmailVerificationToken> EmailVerificationTokens { get; set; } = new List<EmailVerificationToken>();
 }
