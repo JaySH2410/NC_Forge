3e9440f1260ff434bcd58fead153054b87e8ec67
jay sheth <jsheth066@gmail.com>
2026-06-29 13:12:10 +0400

    ADD: refreshToken Entity/Table

---
+++ Infrastructure/Persistence/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using test.Features.Auth.Entities;
+
+namespace test.Infrastructure.Persistence.Configurations;
+
+public class RefreshTokenConfiguration
+    : IEntityTypeConfiguration<RefreshToken>
+{
+    public void Configure(
+        EntityTypeBuilder<RefreshToken> builder)
+    {
+        builder.ToTable("RefreshToken");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.TokenHash)
+            .IsRequired()
+            .HasMaxLength(64);
+
+        builder.Property(x => x.ReplacedByTokenHash)
+            .HasMaxLength(64);
+
+        builder.Property(x => x.RevokedReason)
+            .HasMaxLength(500);
+
+        builder.Property(x => x.ExpiresAt)
+            .IsRequired();
+
+        builder.HasOne(x => x.User)
+            .WithMany(x => x.RefreshTokens)
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasIndex(x => x.TokenHash)
+            .IsUnique();
+
+        builder.HasIndex(x => x.UserId);
+    }
+}
