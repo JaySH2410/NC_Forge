1fcfdd2076f71c492b1b2dc92087ae39589c1232
jay sheth <jsheth066@gmail.com>
2026-06-29 16:58:24 +0400

    ADD: will clean JWT options in future and use Authentication Option

---
+++ Infrastructure/Configuration/AuthenticationOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace test.Infrastructure.Configuration;
+
+public class AuthenticationOptions
+{
+    public const string SectionName = "Authentication";
+
+    public int AccessTokenExpiryMinutes { get; init; }
+
+    public int RefreshTokenExpiryDays { get; init; }
+
+    public int PasswordResetTokenExpiryMinutes { get; init; }
+
+    public int EmailVerificationTokenExpiryHours { get; init; }
+}
