e81cfa58c73d9cf64c1f97f88a67aed14e9edef5
jay sheth <jsheth066@gmail.com>
2026-06-29 13:14:05 +0400

    ADD: Interface for RefreshToken added

---
+++ Features/Auth/Contracts/IRefreshTokenService.cs
@@ -0,0 +1,14 @@
+﻿using test.Features.Auth.DTOs;
+
+namespace test.Features.Auth.Contracts;
+
+public interface IRefreshTokenService
+{
+    RefreshTokenResult GenerateRefreshToken();
+
+    string HashToken(string token);
+
+    bool VerifyToken(
+        string token,
+        string tokenHash);
+}
