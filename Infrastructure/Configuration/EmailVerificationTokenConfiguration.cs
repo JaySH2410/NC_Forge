775976e47b319370401be0bcbc1d3fba2b2abb9d
jay sheth <jsheth066@gmail.com>
2026-06-29 17:04:10 +0400

    ADD: email verification config

---
+++ Infrastructure/Configuration/EmailVerificationTokenConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using test.Features.Auth.Entities;
+
+namespace test.Infrastructure.Persistence.Configurations;
+
+public class EmailVerificationTokenConfiguration
+    : IEntityTypeConfiguration<EmailVerificationToken>
+{
+    public void Configure(
+        EntityTypeBuilder<EmailVerificationToken> builder)
+    {
+        builder.ToTable("EmailVerificationToken");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.TokenHash)
+            .HasMaxLength(64)
+            .IsRequired();
+
+        builder.Property(x => x.ExpiresAt)
+            .IsRequired();
+
+        builder.HasOne(x => x.User)
+            .WithMany(x => x.EmailVerificationTokens)
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasIndex(x => x.TokenHash)
+            .IsUnique();
+
+        builder.HasIndex(x => x.UserId);
+    }
+}
