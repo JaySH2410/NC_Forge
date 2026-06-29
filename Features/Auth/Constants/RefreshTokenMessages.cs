14d5681f3ae6ffac4367883d93a8228231db2c05
jay sheth <jsheth066@gmail.com>
2026-06-29 14:38:54 +0400

    ADD: refresh Toke messages

---
+++ Features/Auth/Constants/RefreshTokenMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace test.Features.Auth.Constants;
+
+public class RefreshTokenMessages
+{
+    public const string TokenRotated = "Token Rotated";
+    public const string UserLoggedOut = "User Logged Out";
+    public const string PasswordChanged = "Password Changed";
+    public const string TokenCompromised = "Token compromised.";
+    public const string AdminRevoked = "Revoked by administrator.";
+}
