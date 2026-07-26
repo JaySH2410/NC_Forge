e1ec5433a2c78f4778bdc52830d39aceba3ecf82
jay sheth <jsheth@HEX-W3152SB4>
2026-07-26 23:20:20 +0400

    UPD: changes to namedentity config

--- Infrastructure/Persistence/Configurations/Base/NamedEntityConfiguration.cs
+++ Infrastructure/Persistence/Configurations/Base/NamedEntityConfiguration.cs
@@ -12,7 +12,13 @@ public abstract class NamedEntityConfiguration<TEntity>
         base.Configure(builder);
 
         builder.Property(x => x.Name)
+            .HasMaxLength(200)
             .IsRequired();
+        builder.Property(x => x.DisplayName)
+            .HasMaxLength(200)
+            .IsRequired();
+        builder.Property(x => x.Description)
+            .HasMaxLength(4000); // or .HasColumnType("nvarchar(max)")
 
         // builder.HasIndex(x => x.Name)
         //     .IsUnique();
