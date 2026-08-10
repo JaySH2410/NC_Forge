cb3d7d115c43929686c3f922004ed589bfe55b34
jay sheth <jsheth066@gmail.com>
2026-08-10 10:59:49 +0400

    UPD: entity config stopped and made simplified as version control and deployment of SQL is being done in SQL project

--- Infrastructure/Persistence/Configurations/MetaSchema/MetaObjectConfiguration.cs
+++ Infrastructure/Persistence/Configurations/MetaSchema/MetaObjectConfiguration.cs
@@ -5,45 +5,10 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Forge.Infrastructure.Persistence.Configurations.MetaSchema;
 
-public class MetaObjectConfiguration: NamedEntityConfiguration<MetaObject>
+public class MetaObjectConfiguration: IEntityTypeConfiguration<MetaObject>
 {
-    public override void Configure(EntityTypeBuilder<MetaObject> builder)
+    public void Configure(EntityTypeBuilder<MetaObject> builder)
     {
-        base.Configure(builder);
-
         builder.ToTable("MetaObject"); 
-        
-        // Map BaseEntity.Uuid -> ObjUid
-        builder.Property(x => x.Uuid)
-            .HasColumnName("ObjUid");
-        
-        builder.HasAlternateKey(x => x.Uuid);
-
-        // Object Type
-        builder.Property(x => x.ObjTypeUid);
-            // .IsRequired();
-
-        builder.Property(x => x.ApplicationUid)
-            .IsRequired();
-
-        builder.HasIndex(x => x.ApplicationUid);
-
-        builder.HasOne<Application>()
-            .WithMany()
-            .HasForeignKey(x => x.ApplicationUid)
-            .HasPrincipalKey(x => x.Uuid)
-            .OnDelete(DeleteBehavior.Restrict);
-
-        builder.Property(x => x.Version)
-            .HasMaxLength(40)
-            .IsRequired();
-
-        builder.HasIndex(x => x.ObjTypeUid);
-
-        builder.HasOne<MetaObject>()
-            .WithMany()
-            .HasForeignKey(x => x.ObjTypeUid)
-            .HasPrincipalKey(x => x.Uuid)
-            .OnDelete(DeleteBehavior.Restrict);
     }
 }
