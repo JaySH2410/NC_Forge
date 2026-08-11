47332926dc8fe30fa068339718c6f516c99e81ed
jay sheth <jsheth066@gmail.com>
2026-08-11 18:44:00 +0400

    ADD: required validators added #11

---
+++ Features/MetaSchema/Validators/CreateMetaObjectRequestValidator.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+using Forge.Features.MetaSchema.DTOs;
+
+namespace Forge.Features.MetaSchema.Validators;
+
+
+public sealed class CreateMetaObjectRequestValidator
+    : AbstractValidator<CreateMetaObjectRequest>
+{
+    public CreateMetaObjectRequestValidator()
+    {
+        RuleFor(x => x.Uuid)
+            .NotEmpty()
+            .WithMessage("Uuid is required.");
+
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Name is required.")
+            .MaximumLength(200)
+            .WithMessage("Name cannot exceed 200 characters.");
+
+        RuleFor(x => x.DisplayName)
+            .MaximumLength(200)
+            .WithMessage("Display Name cannot exceed 200 characters.")
+            .When(x => x.DisplayName is not null);
+
+        RuleFor(x => x.Description)
+            .MaximumLength(4000)
+            .WithMessage("Description cannot exceed 4000 characters.")
+            .When(x => x.Description is not null);
+
+        RuleFor(x => x.ApplicationUid)
+            .NotEmpty()
+            .WithMessage("ApplicationUid is required.");
+
+        RuleFor(x => x.Version)
+            .NotEmpty()
+            .WithMessage("Version is required.")
+            .MaximumLength(40)
+            .WithMessage("Version cannot exceed 40 characters.");
+    }
+}
---
+++ Features/MetaSchema/Validators/UuidRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Forge.Features.MetaSchema.DTOs;
+
+namespace Forge.Features.MetaSchema.Validators;
+
+
+public sealed class UuidRequestValidator
+    : AbstractValidator<UuidRequest>
+{
+    public UuidRequestValidator()
+    {
+        RuleFor(x => x.Uuid)
+            .NotEmpty()
+            .WithMessage("Uuid is required.");
+    }
+}
