b462e8458195f69659cf91ca07eadfdf7270b4cc
jay sheth <jsheth066@gmail.com>
2026-08-11 18:44:41 +0400

    ADD: created the response DTOs #11

---
+++ Features/MetaSchema/DTOs/MetaObjectRelationshipResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace Forge.Features.MetaSchema.DTOs;
+
+
+//used for
+//1. Getting a MetaObjectRelationship by its UUID
+//2. Getting a MetaObjectRelationship by its Name
+//3. Creating a new MetaObjectRelationship
+//4. Updating an existing MetaObjectRelationship
+public sealed class MetaObjectRelationshipResponse {
+    public int Id { get; set; }
+    public Guid Uuid { get; set; }
+    public required string Name { get; set; }
+
+    public string? DisplayName { get; set; }
+
+    public string? Description { get; set; }
+
+    public Guid End1Uid { get; set; }
+
+    public Guid End2Uid { get; set; }
+
+    public Guid RelTypeUid { get; set; }
+
+    public int Ordinal { get; set; }
+}
---
+++ Features/MetaSchema/DTOs/MetaObjectResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace Forge.Features.MetaSchema.DTOs;
+
+//used for
+//1. Getting a MetaObject by its UUID
+//2. Getting a MetaObject by its Name
+//3. Creating a new MetaObject
+//4. Updating an existing MetaObject
+public sealed class MetaObjectResponse
+{
+    public int Id { get; set; }
+    public Guid Uuid { get; set; }
+    public required string Name { get; set; }
+    public string? DisplayName { get; set; }
+    public string? Description { get; set; }
+    public Guid? ObjTypeUid { get; set; }
+    public Guid ApplicationUid { get; set; }
+    public string Version { get; set; } = null!;
+}
