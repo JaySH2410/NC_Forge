e051a75a91a9e261947e7be323b79693f228c292
jay sheth <jsheth066@gmail.com>
2026-08-30 23:09:23 +0400

    ADD: application request validator for CREATE AND UPDATE

--- Features/MetaSchema/Validators/CreateApplicationRequestValidator.cs
+++ Features/MetaSchema/Validators/CreateApplicationRequestValidator.cs
@@ -1,5 +1,33 @@
-﻿namespace Forge.Features.MetaSchema.Validators;
+﻿using FluentValidation;
+using Forge.Features.MetaSchema.DTOs;
 
-public class CreateApplicationRequestValidator
+namespace Forge.Features.MetaSchema.Validators;
+
+public sealed class CreateApplicationRequestValidator
+    : AbstractValidator<CreateApplicationRequest>
 {
+    public CreateApplicationRequestValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Name is required.")
+            .MaximumLength(200)
+            .WithMessage("Name cannot exceed 200 characters.");
+        
+        RuleFor(x => x.DisplayName)
+            .MaximumLength(200)
+            .WithMessage("Display Name cannot exceed 200 characters.")
+            .When(x => x.DisplayName is not null);
+
+        RuleFor(x => x.Description)
+            .MaximumLength(4000)
+            .WithMessage("Description cannot exceed 4000 characters.")
+            .When(x => x.Description is not null);
+        
+        RuleFor(x => x.Version)
+            .NotEmpty()
+            .WithMessage("Version is required.")
+            .MaximumLength(40)
+            .WithMessage("Version cannot exceed 40 characters.");
+    }
 }
