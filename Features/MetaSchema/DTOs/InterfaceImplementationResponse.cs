89f29ab7b7a3444d6021b57a5951822f0b3406a8
jay sheth <jsheth066@gmail.com>
2026-09-05 23:45:29 +0400

    UPD:  relationshipUid added and removed dedicated the primaryRel and interfaceRel and also renamed the IfUid to Uuid

--- Features/MetaSchema/DTOs/InterfaceImplementationResponse.cs
+++ Features/MetaSchema/DTOs/InterfaceImplementationResponse.cs
@@ -3,11 +3,10 @@ namespace Forge.Features.MetaSchema.DTOs;
 public sealed class InterfaceImplementationResponse
 {
     public long Id { get; set; }
-    public Guid IfUid { get; set; }
+    public Guid Uuid { get; set; }
     public Guid ObjUid { get; set; }
     public Guid InterfaceUid { get; set; }
     public bool IsPrimary { get; set; }
     public int Ordinal { get; set; }
-    public Guid ImplementsRelationshipUid { get; set; }
-    public Guid? PrimaryInterfaceRelationshipUid { get; set; }
+    public Guid RelationshipUid { get; set; }
 }
