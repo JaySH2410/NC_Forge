3669ebe7b2e57246bd9862951a64605f788ea5bc
jay sheth <jsheth066@gmail.com>
2026-09-07 17:01:32 +0400

    ADD: Version Increement fro app and obj

---
+++ Features/MetaSchema/Versioning/ApplicationVersionIncrement.cs
@@ -0,0 +1,8 @@
+namespace Forge.Features.MetaSchema.Versioning;
+
+public enum ApplicationVersionIncrement
+{
+    Patch = 1,
+    Minor = 2,
+    Major = 3
+}
---
+++ Features/MetaSchema/Versioning/ObjectVersionIncrement.cs
@@ -0,0 +1,7 @@
+namespace Forge.Features.MetaSchema.Versioning;
+
+public enum ObjectVersionIncrement
+{
+    Minor = 1,
+    Major = 2
+}
